be815e18e2e6273a7800534d65fce6ba4cdf2fa2
Emil2301 <emil.kawalko@vp.pl>
2022-06-21 10:23:42 +0200

    feature/add-styles: configure styled components and add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import GlobalStyles from './styles/Global';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </>
   );
 }
 
---
+++ src/styles/Global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+body {
+    background-color: #E5E5E5;
+    font-size: 62.5%; 
+}
+`;
+
+export default GlobalStyles;
