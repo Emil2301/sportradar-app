dfdd1b96e64b09c07f76d40af61837cad795ed90
Emil2301 <emil.kawalko@vp.pl>
2022-06-21 10:57:32 +0200

    feature/Score-component: add implementation and styles

--- src/pages/simulatedMatches/components/Score/Score.tsx
+++ src/pages/simulatedMatches/components/Score/Score.tsx
@@ -1,5 +1,18 @@
-const Score = () => {
+import { ScoreProps } from './Score.interface';
+import { StyledScore } from '../../../../styles/SimulatedMatches.styled';
+import { FC } from 'react';
 
-}
+const Score: FC<ScoreProps> = ({
+  firstTeamGoalsScored,
+  secondTeamGoalsScored,
+}: ScoreProps) => {
+  return (
+    <StyledScore>
+      <p>
+        {firstTeamGoalsScored}:{secondTeamGoalsScored}
+      </p>
+    </StyledScore>
+  );
+};
 
 export default Score;
--- src/styles/SimulatedMatches.styled.ts
+++ src/styles/SimulatedMatches.styled.ts
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+
+export const StyledScore = styled.div`
+  margin: 0 1rem;
+  p {
+    margin: 0;
+  }
+`;
